beff79a0c3e4b0550fe73c5c1a8d50139d51b2c5
kynmjl <keyan.mujalo@stud.altekanti.ch>
2024-09-27 22:01:08 +0200

    Create vocabularySets.js

---
+++ vocabularySets.js
@@ -0,0 +1,18 @@
+// vocabularySets.js
+const vocabularySets = {
+    set1: [
+        { translation: "House", word: "Haus" },
+        { translation: "Dog", word: "Hund" },
+    ],
+    set2: [
+        { translation: "Car", word: "Auto" },
+        { translation: "Tree", word: "Baum" },
+    ],
+    set3: [
+        { translation: "Book", word: "Buch" },
+        { translation: "Chair", word: "Stuhl" },
+    ]
+};
+
+// Exportiere die Vokabelsets, falls du später mit Modulen arbeitest
+export default vocabularySets;
